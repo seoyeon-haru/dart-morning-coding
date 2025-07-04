62b8b39d7fe4ff7e09cc6befefe3ff5d3d216f02
seoyeon-haru <sy5792@naver.com>
2025-07-04 11:24:52 +0900

    방법 2 함수로 변경

--- lib/code_0703.dart
+++ lib/code_0703.dart
@@ -42,9 +42,19 @@
 // 방법 2 : 새로운 리스트 만들어서 `List`의 길이만큼 반복하면서 반대 값 넣어주기
 void main() {
   List<int> before = [1, 2, 3, 4, 5];
+  // List<int> result2 = [];
+  // for (var i = 0; i < before.length; i++) {
+  //   result2.add(before[before.length - 1 - i]);
+  // }
+  // print(result2);
+  print(reverse1(before));
+  print(before);
+}
+
+List<int> reverse1(List<int> list) {
   List<int> result2 = [];
-  for (var i = 0; i < before.length; i++) {
-    result2.add(before[before.length - 1 - i]);
+  for (var i = 0; i < list.length; i++) {
+    result2.add(list[list.length - 1 - i]);
   }
-  print(result2);
+  return result2;
 }
