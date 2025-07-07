a1edc1c5b70a60d72952a85ea0ddfb0a1dded510
seoyeon-haru <sy5792@naver.com>
2025-07-07 11:47:36 +0900

    242.유효한 철자 바꾸기

---
+++ lib/code_0707.dart
@@ -0,0 +1,28 @@
+void main() {
+  String s = "abc";
+  String t = "adc";
+  print(isAnagram(s, t));
+}
+
+/// 서로 같은 알파벳으로만 구성되어 있는지!
+bool isAnagram(String s, String t) {
+  // 1. 글자들을 각각 한글자씩 나누기 => List
+  List<String> sSplited = s.split('');
+  List<String> tSplited = t.split('');
+
+  // 2. 각각 정렬하기
+  sSplited.sort();
+  tSplited.sort();
+
+  // 3. 비교
+  // 첫 번째. for 문 돌면서 비교하기
+  // for (var i = 0; i < sSplited.length; i++) {
+  //   if (sSplited[i] != tSplited[i]) {
+  //     return false;
+  //   }
+  // }
+  // return true;
+  
+  // 두 번째. 글자 다시 하나의 문자열로 변환한 뒤 비교
+  return sSplited.join() == tSplited.join();
+}
