4b286d5dad6a7314d722f8d06200e93443d16d3a
seoyeon-haru <sy5792@naver.com>
2025-07-18 11:25:46 +0900

    7/18 문자열 개수 카운트

---
+++ lib/code_0718.dart
@@ -0,0 +1,22 @@
+void main() {
+  print("Hello world ${example('hellog world')}");
+  print("seoyeon haru ${example('seoyeon haru')}");
+}
+
+// 문자열 str을 전달받아서
+// 각 글자가 몇 번 포함되어 있는지 맵에 저장 후 반환
+// "hello world" => {"h" : 1, "e" : 1, "l" : 3, "o" : 2, "d" : 1, " " : 1}
+// 반환 타입 Map<String, int>
+// 함수 이름 example
+// 함수 인자 String str
+Map<String, int> example(String str) {
+  List<String> chars = str.split('');
+  Map<String, int> result = {};
+  for (var i = 0; i < chars.length; i++) {
+    String char = chars[i];
+    // result 안에 char 키에 해당하는 값이 없을 경우 1로, 있을 경우 +1
+    int prevValue = result[char] ?? 0;
+    result[char] = prevValue + 1;
+  }
+  return result;
+}
